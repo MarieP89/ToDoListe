c962cb33556e4cd4b1f1598df2f5718e22d8dd54
mpotgeter <marie.potgeter@endava.com>
2024-02-19 20:47:39 +0100

    Initial commit

---
+++ components/ButttonInput.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default{
+  data(){
+    return{
+      showInput: false,
+    }
+  },
+}
+</script>
+
+
+<template>
+    <UButton color="primary" @click="showInput =! showInput">Add Label</UButton>
+    <UInput v-if="showInput" class="mt-2 p-2 border rounded w-96"  type="text">
+      <UButton icon="i-heroicons-plus-small-20-solid"/>
+    </UInput>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import ButttonInput from "~/components/ButttonInput.vue";
+
+const colorMode = useColorMode()
+const task = ref('')
+
+const links = computed(() => [{
+  label: 'Home',
+  icon: 'i-heroicons-academic-cap'
+}, {
+
+  icon: colorMode.value === 'light' ? 'i-heroicons-sun' : 'i-heroicons-moon',
+  click: () => {
+    colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'
+  }
+}])
+
+</script>
+
+
+<template>
+  <UHorizontalNavigation :links="links"/>
+  <div>
+    <h1 class="text-3xl font-bold text-center my-10">
+      ToDo List
+    </h1>
+  </div>
+  <UContainer>
+    <UInput v-model="task" class=""/>
+    <UButton @click="addTask">Add Task</UButton>
+  </UContainer>
+  <ButttonInput/>
+</template>
