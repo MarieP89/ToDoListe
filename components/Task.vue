32719423af931b7a1086101ad9385c6a38cdf9aa
mpotgeter <marie.potgeter@endava.com>
2024-02-22 16:10:06 +0100

    can now select a Label in a dropdown

--- components/Task.vue
+++ components/Task.vue
@@ -1,10 +1,25 @@
 <script setup>
-const task = ref('')
+const task = ref('');
+const labels = ref([]);
+const selectedLabel = ref('')
+
+async function fetchLabels(){
+  try {
+    const response = await $fetch('/api/label/getLabel');
+    labels.value = response.body.map( label => ({label: label.name, value:label.id}));
+  }catch (error){
+    console.log("Error fetching labels", error);
+  }
+}
+onMounted(fetchLabels)
 
 </script>
 <template>
   <UContainer>
-    <UInput v-model="task" class=""/>
-    <UButton @click="addTask">Add Task</UButton>
+    <div class="flex space-x-4">
+      <UInputMenu v-model="selectedLabel" :options="labels"></UInputMenu>
+      <UInput v-model="task" class=""/>
+      <UButton @click="addTask">Add Task</UButton>
+    </div>
   </UContainer>
 </template>
