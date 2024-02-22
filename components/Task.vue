7c944b5da41c4b9c34ba30368a318a56909f13d2
mpotgeter <marie.potgeter@endava.com>
2024-02-22 17:39:48 +0100

    added button to delete selected label

--- components/Task.vue
+++ components/Task.vue
@@ -3,21 +3,39 @@ const task = ref('');
 const labels = ref([]);
 const selectedLabel = ref('')
 
-async function fetchLabels(){
+async function fetchLabels() {
   try {
     const response = await $fetch('/api/label/getLabel');
-    labels.value = response.body.map( label => ({label: label.name, value:label.id}));
-  }catch (error){
+    labels.value = response.body.map(label => ({label: label.name, value: label.id}));
+  } catch (error) {
     console.log("Error fetching labels", error);
   }
 }
+
 onMounted(fetchLabels)
+
+function clearSelected() {
+  selectedLabel.value = '';
+}
 
 </script>
 <template>
   <UContainer>
     <div class="flex space-x-4">
-      <UInputMenu v-model="selectedLabel" :options="labels"></UInputMenu>
+      <UInputMenu v-model="selectedLabel" :options="labels">
+        <template #trailing>
+          <div class="flex items-center">
+            <UButton
+                v-show="selectedLabel !== ''"
+                :padded="false"
+                color="gray"
+                icon="i-heroicons-x-mark-20-solid"
+                variant="link"
+                @click="clearSelected">
+            </UButton>
+          </div>
+        </template>
+      </UInputMenu>
       <UInput v-model="task" class=""/>
       <UButton @click="addTask">Add Task</UButton>
     </div>
