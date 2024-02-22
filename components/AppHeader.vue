7e8079facf4533f55ecc5b638664d36ff70e8942
mpotgeter <marie.potgeter@endava.com>
2024-02-22 16:15:33 +0100

    colorMode outside computed

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,17 +1,15 @@
 <script setup>
 
 const colorMode = useColorMode()
-
-
+const toggleColorMode = () => {
+  colorMode.value = colorMode.value === 'light' ? 'dark' : 'light';
+};
 const links = computed(() => [{
   label: 'Home',
   icon: 'i-heroicons-home'
 }, {
-
   icon: colorMode.value === 'light' ? 'i-heroicons-sun' : 'i-heroicons-moon',
-  click: () => {
-    colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'
-  }
+  click: toggleColorMode
 }])
 
 </script>
